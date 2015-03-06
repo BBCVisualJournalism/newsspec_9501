31500e0611252847f076b886ceb5f5cb38e0891c
fewstera <aidan.fewster@bbc.co.uk>
2015-03-06 12:13:47 +0000

    Added week 16 and 17 - FUNNY HOW WE HAD TO ADD WEEK 16 ASWELL @mupini ;)

--- source/js/module/views/bar_charts.js
+++ source/js/module/views/bar_charts.js
@@ -15,8 +15,8 @@ define(["lib/news_special/bootstrap"],  function (news) {
         that,
         WINTER_PRESSURES_START_DATE = "(3-9 Nov)",
         AEWEEKLY_SITREPS_START_DATE = "(3-9 Nov)",
-        WINTER_PRESSURES_END_DATE = "(9-15 Feb)",
-        AEWEEKLY_SITREPS_END_DATE = "(9-15 Feb)";
+        WINTER_PRESSURES_END_DATE = "(23 Feb - 1 Mar)",
+        AEWEEKLY_SITREPS_END_DATE = "(23 Feb - 1 Mar)";
 
     var BarChart = function () {
         that = this;
